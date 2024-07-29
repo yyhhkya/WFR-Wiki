e5ca29e225d3865d6bb020235fadc342da93ee56
yyhhkya <i@hiwer.cn>
2024-07-29 19:27:52 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,28 +13,40 @@ export default defineUserConfig({
   theme: defaultTheme({
     logo: '/img/logo.png',
 
+    // 顶部导航
     navbar: [
       { text: '首页', link: '/' },
-      
+
       {
         text: '守则',
         children: [
-          { text: '服务器玩家守则', link: '/服务器玩家守则' },
-          { text: 'QQ群群员守则', link: '/QQ群群员守则' },
-          { text: 'git-test', link: '/git-test' },
+          { text: '服务器玩家守则', link: '/守则/服务器玩家守则' },
+          { text: 'QQ群群员守则', link: '/守则/QQ群群员守则' },
+          // { text: 'git-test', link: '/git-test' },
         ]
       },
-    ]
+
+      { text: '游玩教程', link: '/游玩教程' },
+
+      { text: '常见问题', link: '/常见问题' },
+
+    ],
+
+    repo: "https://github.com/yyhhkya/WFR-Wiki",
 
   }),
 
-  bundler: viteBundler(),
-
+  // repo: "https://gitlab.com/yyhhkya/WFR-Wiki",
+  // docsRepo: 'https://gitlab.com/yyhhkya/WFR-Wiki',
+  // docsBranch: 'master',
+  // docsDir: 'docs',
+  // editLinkPattern: ':repo/-/edit/:branch/:path',
+  
   // 插件
   plugins: [
-    // gitPlugin({
-    //   lastUpdated: "更新时间",
-    // }),
+
   ],
+
+  bundler: viteBundler(),
 
 })
